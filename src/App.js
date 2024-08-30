522f10dc85bf096c2a3eea8040020b8b747cf5ac
Ryan Tracey <ryan.tracey@IC-R23Q6NYFK6.local>
2024-08-30 10:57:40 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          The customer has asked us to create a simplified calculator app.
+        </p>
+        <ul className="App-instructions">
+          <li>It should take two numbers and display the mathematical result</li>
+          <li>It should default to addition</li>
+          <li>It can also perform subtraction</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
